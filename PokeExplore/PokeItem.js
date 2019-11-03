3223994cebdc8f22e0d2f410b23fa49ab525c521
Eve Stockstill <evestockstillpdx@gmail.com>
2019-11-02 22:11:48 -0700

    fixed travis

--- PokeExplore/PokeItem.js
+++ PokeExplore/PokeItem.js
@@ -5,13 +5,13 @@ class PokeItem extends Component {
         const poke = this.props.poke;
         return `
                     <li class="poke-list">
-                           <div>
+                        <div>
                             <img src="${poke.url_image}">
                             <div class="poke-cards">
                                 <h3 class="poke-name">${poke.pokemon}</h3>
                                 <h2>They are of the type ${poke.type_1}</h2>
                                 <p>This pokemon has a defense of ${poke.defense} and attack is at ${poke.attack} The egg group is ${poke.egg_group_1}</p>
-                               </div>
+                        </div>  
                         </div>
                     </li>
         `;
--- test/PokeItem.test.js
+++ test/PokeItem.test.js
@@ -10,14 +10,15 @@ test('renders html from data', assert => {
     };
 
     const expected = /*html*/`
-         <li>
-                        <div class="poke-pic">
-                            <img src="${poke.url_image}http://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png alt="pokemon">
+        
+                    <li class="poke-list">
+                        <div>
+                            <img src="${poke.url_image}">
                             <div class="poke-cards">
                                 <h3 class="poke-name">${poke.pokemon}</h3>
                                 <h2>They are of the type ${poke.type_1}</h2>
                                 <p>This pokemon has a defense of ${poke.defense} and attack is at ${poke.attack} The egg group is ${poke.egg_group_1}</p>
-                            </div>
+                        </div>  
                         </div>
                     </li>
     `;
